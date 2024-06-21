2a18090961fee25b24d3f71495befecada9a5460
Valeria220499 <20170027@est.umss.edu>
2024-06-21 18:17:38 -0400

    Agregando 3 EndPoint de museo de Valeria Caritas

---
+++ Test/Test_Pablo_Picasso.py
@@ -0,0 +1,7 @@
+import pytest
+import requests
+
+def test_get():
+    url = "https://www.metmuseum.org/es/art/collection/search?q=pablo%20picasso"
+    response = requests.get(url)
+    assert response.status_code == 200

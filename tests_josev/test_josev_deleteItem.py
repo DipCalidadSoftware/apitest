760f9090ffaa4bec12aff7e89ce62877592f611b
josevsoft <josevsoft@gmail.com>
2024-06-23 21:50:25 -0400

    apirest-001/josevallejos Upload task1crud-centralizado

--- tests_josev/test_josev_deleteItem.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-def test_delete_item():
-    """This test request the POST method for remove the bag items"""
-    url = "https://store.metmuseum.org/checkout/sidebar/removeItem/"
-    response = requests.post(url)
-    assert response.status_code == 200
--- tests_josev/test_josev_updateItem.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-def test_update_item():
-    """This test request the POST method for updating article from cart - removed 1 article"""
-    url = "https://store.metmuseum.org/checkout/sidebar/updateItemQty/?cartQtyUpdate=true"
-    response = requests.post(url)
-    assert response.status_code == 200

81c6454dc563a947f65bf76b1fe78e3c9a6214e0
Valeria220499 <20170027@est.umss.edu>
2024-06-24 16:13:07 -0400

    3 endpoint museo Valeria Caritas

--- Test/Test_Claude_Monet.py
+++ Test/Test_Claude_Monet.py
@@ -1,6 +1,6 @@
 import pytest
 import requests
-
+pytest
 def test_get():
     url = "https://www.metmuseum.org/es/art/collection/search?q=claude%20monet"
     response = requests.get(url)
--- Test/Test_Max_Liebermann.py
+++ Test/Test_Max_Liebermann.py
@@ -1,6 +1,6 @@
 import pytest
 import requests
-
+pytest
 def test_get():
     url = "https://www.metmuseum.org/es/art/collection/search?q=max%20liebermann"
     response = requests.get(url)

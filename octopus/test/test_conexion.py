de54a839f4aee83947bc353fe840d36b8d7c9b63
juanpa <201400076@est.umss.edu>
2024-06-25 20:18:57 -0400

    TRELLO-004:Login, configuracion de variables de entorno

--- octopus/test/test_conexion.py
+++ octopus/test/test_conexion.py
@@ -1,9 +1,18 @@
+from decouple import config
 import requests
+id = config('ID')
+key = config('KEY')
+url = config('URL')
+token = config('TOKEN')
 
-ENDPOINT = "https://api.trello.com/1/cards/66712276ba3e713a97a105fb?key=39ad1240991f892d98ca7a43e516aa5e&token=ATTA7e6858c9c83947b73587f5ad3cf9f175a13eb9f95b821a3f78af068c07a46d1bABDDB9FB"
+ENDPOINT = f"{url}/1/cards/{id}?key={key}&token={token}"
 
 def test_can_all_endpoint():
     response = requests.get(ENDPOINT)
+    print('asdsad')
     assert response.status_code == 200
     
-    
+def test_can_all_endpoint2():
+    response = requests.get(ENDPOINT)
+    print('asdsad')
+    assert response.status_code == 200

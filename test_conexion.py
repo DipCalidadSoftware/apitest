8dac9c3d8de44d7fc3f27ff2566a5496576bad90
automation-code-api <110135947+automation-code-api@users.noreply.github.com>
2024-07-08 18:25:58 -0400

    Update test_conexion.py

--- test_conexion.py
+++ test_conexion.py
@@ -1,10 +1,4 @@
 import requests
-import config
 
-ENDPOINT = "https://api.trello.com/1/cards/"+config.ID+"?key="+config.API_KEY+"&token="+config.TOKEN
-
-def test_can_all_endpoint():
-    response = requests.get(ENDPOINT)
-    assert response.status_code == 200
     
     
